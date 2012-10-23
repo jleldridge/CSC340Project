3cffde6e063760c181c3f137c02891917faf2df0
Luke Eldridge <jleldrid@uncg.edu>
2012-10-23 12:13:09 -0400

    Slightly changed FamilyMatcher and added diagrams to version control.

--- CSC340Project/src/matchingAlgorithm/FamilyMatcher.java
+++ CSC340Project/src/matchingAlgorithm/FamilyMatcher.java
@@ -12,4 +12,15 @@ public class FamilyMatcher {
 	public FamilyMatcher(ArrayList<Family> families){
 		this.families = families;
 	}
+	
+	
+	public ArrayList<Family> getPossibleMatches(Family f){
+		//add all families to the possible matches to start
+		ArrayList<Family> possibleMatches = new ArrayList<>(families);
+		//remove the family we are finding matches for
+		possibleMatches.remove(f);
+		
+		
+		return possibleMatches;
+	}
 }
